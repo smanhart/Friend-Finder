07d0474cd90412e907f188617cd6475c92cf84e1
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-12-29 19:26:45 -0700

    pulling data from html, working on post logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,8 @@ module.exports = function(app) {
     })
 
     app.post("/api/friends", function(req, res) {
+        var scores = [];
+
         
-    })
+    }
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,8 +3,8 @@ var path = require("path");
 module.exports = function(app) {
 
     app.get("/survey", function(req, res) {
-        // res.sendFile(path.join(__dirname + "/../public/survey.html"))
-        res.send("It's also working")
+        res.sendFile(path.join(__dirname + "/../public/survey.html"))
+        // res.send("It's also working")
     })
 
     app.use(function(req, res) {
