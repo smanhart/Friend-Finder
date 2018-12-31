35be60ccf8d99ed380beafd602eb21ff2ffb79f2
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-12-30 20:48:58 -0700

    got numbers into array, added to friends list

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,55 @@ module.exports = function(app) {
     })
 
     app.post("/api/friends", function(req, res) {
-        var scores = [];
-
+        var person = req.body;
+        var numList = [
+            parseInt(person.questionOne),
+            parseInt(person.questionTwo),
+            parseInt(person.questionThree),
+            parseInt(person.questionFour),
+            parseInt(person.questionFive),
+            parseInt(person.questionSix),
+            parseInt(person.questionSeven),
+            parseInt(person.questionEight),
+            parseInt(person.questionNine),
+            parseInt(person.questionTen),
+        ];
         
-    }
+        var profile = {
+            name: person.name,
+            photo: person.picture,
+            scores: numList
+        }
+
+        friendArray.push(profile);
+
+
+        // var numArray = person.filter(function(obj) {
+        //     if (obj.value !== isNaN) {
+        //         scores.push(numArray);
+        //     }
+        // })
+
+        // var numArray = numList.map(function(obj) {
+        //     var num = obj.value;
+        //     return num;
+        // })
+
+        // console.log(numList);
+        res.json(person);
+
+
+
+
+
+
+
+        //change this to return the data that will display in the pop up module
+            // for (var i = 0; i < characters.length; i++) {
+            //     if (chosen === characters[i].routeName) {
+            //       return res.json(characters[i]);
+            //     }
+            //   }
+        
+    })
 }
