afde6a7d740446c5535d593e00b75895afb66f8e
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2019-01-07 13:31:14 -0700

    program fully working

--- app/data/friends.js
+++ app/data/friends.js
@@ -11,8 +11,40 @@ var friendArray = [
             5,
             3,
             2,
-            3,
+            5,
             4
+        ]
+    },
+    {
+        name: "Gerald",
+        photo: "https://mymodernmet.com/wp/wp-content/uploads/archive/4FGlDpiZmxDv3inNVp7f_tophybrophy10.jpg",
+        scores: [
+            5,
+            2,
+            1,
+            1,
+            3,
+            3,
+            2,
+            4,
+            3,
+            2
+        ]
+    },
+    {
+        name:"Ada",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Ada_Lovelace_portrait_circa_1840.jpg",
+        scores: [
+            4,
+            3,
+            3,
+            4,
+            5,
+            5,
+            5,
+            3,
+            4,
+            3
         ]
     }
 ]
